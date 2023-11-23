9dc837762fcee1feeb2f2938282c223529849a12
Vince <112585869+vinn03@users.noreply.github.com>
2023-11-22 16:14:54 -0800

    markers and map added

--- NCT/src/app/app.module.ts
+++ NCT/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DataTableComponent } from './data-table/data-table.component';
 import { CreateReportComponent } from './create-report/create-report.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataTableComponent,
-    CreateReportComponent
+    CreateReportComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
--- NCT/src/app/map/map.component.ts
+++ NCT/src/app/map/map.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
+export class MapComponent implements OnInit{
+  
+    private map!: L.Map;
 
+    constructor() {
+    }
+
+    ngOnInit(): void {
+      this.showMap();
+      this.map.on('click', this.addMarker.bind(this));
+    }
+
+    showMap(): void {
+      this.map = L.map('mapid').setView([49.27, -123], 11);
+
+      const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(this.map);
+    }
+
+    addMarker(e: L.LeafletMouseEvent): void {
+      L.marker([e.latlng.lat, e.latlng.lng]).addTo(this.map);
+    }
+
+    
 }
